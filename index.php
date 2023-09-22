5d12b65b808cbd4bd30b1660dc2bf503e4c17b3d
akandeseun <akandeseun44@gmail.com>
2023-09-22 17:27:54 +0100

    pdo basics

--- index.php
+++ index.php
@@ -1,3 +1,43 @@
 <?php
 
-require "router.php";
+// require "router.php";
+
+require "functions.php";
+
+
+// brief review on classes
+
+// class Person
+// {
+//   public $name;
+//   public $age;
+
+//   public function breathe()
+//   {
+//     echo $this->name . " is breathing";
+//   }
+// }
+
+// $person = new Person();
+
+// $person->name = 'Seun Akande';
+// $person->age = 30;
+
+// dieAndDump($person->breathe());
+
+// ----------------
+// connect to our MySQL database.
+
+// Step 1 - initialize PDO (create a new instance of the PDO class)
+
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4;user=root;password=akandeseun44;";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll();
+
+dieAndDump($posts);
